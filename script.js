16f60df7400f0fa3c3280a51381db6318328f4c1
Dmitriy <test@mail.com>
2025-01-26 19:58:56 +0500

    addTrimMethot

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const personalMovieDB = {
 
 function rememberMyFilms(){
     for (let i = 0; i < 2; i++){
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(), //trim убирает пробелы
         b = prompt('На сколько оцените его?', '');
         if (a != null && b != null && a != '' && b != '' && a.length < 50){
             console.log('done');
